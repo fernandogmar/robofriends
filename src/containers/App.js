d48f9bd1e9884ee82266612c39d8e78b568b0639
fernandogmar <fernandogmar@users.noreply.github.com>
2024-02-27 07:28:34 +0100

    polished robofriends version

---
+++ src/containers/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import CardDeck from '../components/CardDeck';
+import Scroll from '../components/Scroll';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+
+class App extends Component {
+    /*constructor() {
+        super();
+
+        this.state = {
+            robots: [],
+            searchfield: ''
+        };
+    }*/
+
+    state = {
+        robots: [],
+        searchfield: ''
+    }
+
+    componentDidMount() {
+        /*this.setState({
+            robots: await fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+        });*/
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then(robots => this.setState({ robots }))
+    }
+
+    handleSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(
+            (robot) => robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        );
+
+        if(robots.length) {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox onSearchChange={this.handleSearchChange} />
+                    <Scroll>
+                        <CardDeck robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
+
+        return (
+            <div className="tc">
+                <h1 className="f1">Loading...</h1>
+            </div>
+        );
+
+    }
+};
+
+export default App;
